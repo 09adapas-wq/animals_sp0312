f04f8d22df7e8f8f7d281b4a94765d8f9e97b589
a-ben-edu <anders.bengtsson@edu.boras.se>
2026-03-12 12:06:26 +0100

    Create animals.py

---
+++ animals.py
@@ -0,0 +1,17 @@
+def main():
+    while True:
+        ## Skriv ut menyvalen
+        print("1. Cat")
+        print("Enter x to exit")
+
+        choice = input("Enter your selection")
+        match choice.lower():
+            case '1': #Cat-sound
+                print("Mjau, mjau")
+            case 'x': #Exit
+                break
+            case _:
+                print(f"Unrecognized input {choice}")
+
+if __name__ == '__main__':
+    main()
